75ca5c245fda561e318f53620f27aff838263f9f
Nikolai Janakiev <niko.email@rocketmail.com>
2016-10-28 20:43:54 +0200

    gh-pages initial commit

--- hello.js
+++
@@ -1,9 +0,0 @@
-console.log("Start Bot");
-
-var Twit = require('twit');
-var client = new Twit(require('./config'));
-
-client.post('statuses/update', { status: 'Test!' }, 
-	function(err, data, response) {
-		console.log(data)
-	});
